62ead0cb96ce2400d19ea7dfa5cb179e28c144fd
lazyweirdo <marcelovillarrealfasanelli@gmail.com>
2019-12-16 16:20:38 +0100

    Update app.js
    yes, clever :) ... maybe Date.now(); for short ?
    
    Co-Authored-By: Marcin Rataj <lidel@lidel.org>

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ Status.prototype.check = function() {
 
 	// we set a random number as a url parameter, to try to prevent content caching
 	// is it right ? ... do you know a better way ? ... does it always work ?
-	let rnd = Math.random();
+	let rnd = new Date()).getTime();
 
 	// 3 important things here
 	//   1) we add #x-ipfs-companion-no-redirect to the final url (self explanatory)
@@ -180,4 +180,3 @@ function checkGateways (gateways) {
 fetch('./gateways.json')
 	.then(res => res.json())
 	.then(gateways => checkGateways(gateways));
-

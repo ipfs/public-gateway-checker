f5fc7235e20190472413e2f3557269cfa77f6445
Nishant Arora <1895906+whizzzkid@users.noreply.github.com>
2022-10-21 11:04:39 -0600

    fix: flag column and new ipfs-geoip dataset (#319)
    Also, removing ipfs-http-client

--- src/constants.ts
+++ src/constants.ts
@@ -4,8 +4,10 @@ const IMG_HASH = 'bafybeibwzifw52ttrkqlikfzext5akxu7lz4xiwjgwzmqcpdzmp3n5vnbe' /
 // const IFRAME_HASH = 'bafkreifx3g6bkkwl7b4v43lvcqfo5vshbiehuvmpky2zayhfpg5qj7y3ca'
 const HASH_STRING = 'Hello from IPFS Gateway Checker'
 const TRUSTLESS_RESPONSE_TYPES = ['raw', 'car']
+const DEFAULT_IPFS_GATEWAY = 'https://ipfs.io'
 
 export {
+  DEFAULT_IPFS_GATEWAY,
   HASH_STRING,
   HASH_TO_TEST,
   // IFRAME_HASH,

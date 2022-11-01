d9faed7fdf5a3b6b197b3a4ed2c8371fea2397b8
lidel <lidel@lidel.org>
2022-11-01 19:31:55 +0000

    Update gh-pages to output generated at 79bb51d

--- src/Ipns.js
+++ src/Ipns.js
@@ -16,7 +16,7 @@ class IPNSCheck extends CheckBase {
         // Since gateway URLs are hard coded with /ipfs/, we need to parse URLs and override the path to /ipns/.
         const gatewayUrl = new URL(this.parent.gateway);
         gatewayUrl.pathname = IPNS_PATH_TO_TEST;
-        const testUrl = `${gatewayUrl.href}?now=${now}`;
+        const testUrl = `${gatewayUrl.href}?now=${now}#x-ipfs-companion-no-redirect`;
         try {
             const response = await fetch(testUrl);
             if (response.status === 200) {

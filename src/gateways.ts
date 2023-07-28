35103e36de1e332cb7e8a71c3acd59960c74ad44
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2023-07-28 11:46:21 -0700

    fix(gateways.ts): trailing comma

--- src/gateways.ts
+++ src/gateways.ts
@@ -84,5 +84,5 @@ export default [
   'https://ipfs.soul-network.com/ipfs/:hash',
   'https://ipfs.twdragon.net/ipfs/:hash',
   'https://getipfs.com/ipfs/:hash',
-  'https://storage.web3ph.dev/gw/:hash'
+  'https://storage.web3ph.dev/gw/:hash',
 ]

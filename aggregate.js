4be2a908a3c000bf5e09a7e4ef21f3c38fe279b0
Laurent Senta <laurent@singulargarden.com>
2023-08-28 09:51:26 +0200

    feat: add aggregate

---
+++ aggregate.js
@@ -0,0 +1,58 @@
+const fs = require('fs')
+const process = require('process')
+
+// params: list of json reports to aggregate
+const inputs = process.argv.slice(2)
+
+const processReport = (filePath) => {
+  const json = require(filePath)
+
+  // extract the TestMetadata
+  const { TestMetadata, ...rest } = json
+  const { time, meta } = TestMetadata
+  const { version, job_url, gateway_url } = meta
+
+  // and the data we care about.
+  // keep only the tests at the root
+  const entries = Object.entries(rest)
+  const roots = entries
+    .filter(([_, value]) => value.path.length === 1)
+    .map(([key, value]) => {
+      const result = {
+        outcome: value.outcome,
+      }
+
+      return [key, result]
+    })
+
+  return [
+    gateway_url,
+    {
+      metadata: {
+        time, version, job_url, gateway_url,
+      },
+      results:
+        Object.fromEntries(
+          roots,
+        ),
+    },
+  ]
+}
+
+const main = async () => {
+  const results = {}
+
+  inputs.forEach((filePath) => {
+    const [name, report] = processReport(filePath)
+    results[name] = report
+  })
+
+  fs.writeFileSync(1, JSON.stringify(results, null, 2))
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.error(err)
+    process.exit(1)
+  })

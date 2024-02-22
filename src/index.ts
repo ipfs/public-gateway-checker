fde7c0d39824e951ce6056903c4168b431bdb25d
Daniel Norman <1992255+2color@users.noreply.github.com>
2024-02-22 11:35:32 +0100

    Decouple frontend gateway list from conformance tests results (#533)
    * chore: decouple gateway list from conformance tests
    
    Related #530
    Also switch to json for easier parsing
    
    * chore: fix linting error
    
    * ci: fix linting
    
    * Update README.md
    
    Co-authored-by: Marcin Rataj <lidel@lidel.org>
    
    ---------
    
    Co-authored-by: Daniel N <2color@users.noreply.github.com>
    Co-authored-by: Marcin Rataj <lidel@lidel.org>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { Workbox } from 'workbox-window'
+import gateways from '../gateways.json'
 import { Checker } from './Checker'
 import { Log } from './Log'
 import { loadCountly } from './metrics'
-import report from './report.json'
 
 const wb = new Workbox('/sw.js')
 void wb.register()
@@ -11,8 +11,6 @@ loadCountly()
 const log = new Log('App index')
 
 window.checker = new Checker()
-
-const gateways = Object.keys(report)
 
 window.checker.checkGateways(gateways).catch((err) => {
   log.error('Unexpected error')
